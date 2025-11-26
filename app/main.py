4e1a8443b63580d8db607c6b132b17ae22cd9f8c
fatalityHub <truu666@gmail.com>
2025-11-26 10:31:45 +0300

    added docker,docker-compose, nginx

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
-from fastapi import FastAPI
+from uuid import uuid4
+
+from fastapi import FastAPI, Request
+from loguru import logger
+from fastapi.responses import JSONResponse
 
 from app.routers import categories, products, users, reviews
+
+logger.add("info.log", format="Log: [{extra[log_id]}:{time} - {level} - {message}]",
+           level="INFO", enqueue=True)
 
 # Создаём приложение FastAPI
 app = FastAPI(
@@ -8,12 +15,30 @@ app = FastAPI(
     version="0.1.0",
 )
 
+
+@app.middleware("http")
+async def log_middleware(request: Request, call_next):
+    log_id = str(uuid4())
+    with logger.contextualize(log_id=log_id):
+        try:
+            response = await call_next(request)
+            if response.status_code in [401, 402, 403, 404]:
+                logger.warning(f"Request to {request.url.path} failed")
+            else:
+                logger.info('Successfully accessed ' + request.url.path)
+        except Exception as ex:
+            logger.error(f"Request to {request.url.path} failed: {ex}")
+            response = JSONResponse(content={"success": False}, status_code=500)
+        return response
+
+
 # Подключаем маршруты категорий и товаров
 app.include_router(categories.router)
 app.include_router(products.router)
 app.include_router(users.router)
 app.include_router(reviews.router)
 app.include_router(reviews.other_router)
+
 
 # Корневой эндпоинт для проверки
 @app.get("/")
--- app/routers/reviews.py
+++ app/routers/reviews.py
@@ -1,5 +1,3 @@
-from itertools import product
-
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -7,7 +5,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.db_depends import get_async_db
 from app.models.reviews import Review as ReviewModel
 from app.models.products import Product as ProductModel
-from app.schemas import Product as ProductSchema, ProductCreate
 from app.schemas import ReviewSchema, ReviewCreate
 from app.models.users import User as UserModel
 from app.auth import get_current_buyer, get_current_admin
